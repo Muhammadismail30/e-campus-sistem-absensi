8561b2053356e82da6749c84cb506d45e704edd4
Muhammadismail30 <mhmmdismailhaji@gmail.com>
2025-05-17 00:13:07 +0700

    perbaiki dashboard, dan menambahkan folder admin,dosen,mahasiswa

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -8,10 +8,10 @@
             <div class="hidden md:block">
                 <div class="ml-10 flex items-baseline space-x-4">
                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                <a href="/home" class="{{ request()->is('home') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Dashboard</a>
-                <a href="/matakuliah" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Mata kuliah</a>
-                <a href="/jadwal" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Jadwal</a>
-                <a href="/presensi" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Presensi</a>
+                <a href="/home" class="{{ request()->is('home') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white" aria-current="page">Dashboard</a>
+                <a href="/matakuliah" class="{{ request()->is('matakuliah') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Mata kuliah</a>
+                <a href="/jadwal" class="{{ request()->is('jadwal') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Jadwal</a>
+                <a href="/presensi" class="{{ request()->is('presensi') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Presensi</a>
                 </div>
             </div>
             </div>
