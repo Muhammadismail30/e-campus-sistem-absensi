f403fa119c2b8daa315cf8e887290350ec1e1422
rthoriq <raihanalif145@gmail.com>
2025-06-04 01:21:49 +0800

    tampilan presensi (mahasiswa)

--- app/Http/Controllers/Mahasiswa/PresensiController.php
+++ app/Http/Controllers/Mahasiswa/PresensiController.php
@@ -11,21 +11,26 @@ class PresensiController extends Controller
 {
     public function index()
     {
-        $mahasiswaId = Auth::id(); 
+    $mahasiswaId = Auth::id();
 
+    // Ambil semua presensi yang diikuti mahasiswa
+    $presensi = Presence::with(['mataKuliah', 'attendances' => function ($query) use ($mahasiswaId) {
+        $query->where('mahasiswa_id', $mahasiswaId);
+    }])
+    ->whereHas('attendances', function ($query) use ($mahasiswaId) {
+        $query->where('mahasiswa_id', $mahasiswaId);
+    })
+    ->orderByDesc('tanggal')
+    ->get();
 
-        $presensi = Presence::with(['mata_Kuliah', 'attendances' => function ($query) use ($mahasiswaId) {
-            $query->where('mahasiswa_id', $mahasiswaId);
-        }])
-        ->whereHas('attendances', function ($query) use ($mahasiswaId) {
-            $query->where('mahasiswa_id', $mahasiswaId);
-        })
-        ->orderByDesc('updated_at')
-        ->get();
+    // Ambil absensi aktif terbaru
+    $activeAbsensi = Presence::where('is_active', true)->latest()->first();
 
-        return view('mahasiswa.presensi', [
-            'title' => 'Riwayat Presensi',
-            'presensi' => $presensi,
-        ]);
-    }
+    return view('mahasiswa.presensi', [
+        'title' => 'Riwayat Presensi Mahasiswa',
+        'presensi' => $presensi,
+        'activeAbsensi' => $activeAbsensi, // ← INI YANG PENTING
+    ]);
+}
+
 }
